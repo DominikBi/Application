7d81986ee6c74f367cdb4fb988438497c69f2b22
DominikBi <birkeldominik@gmail.com>
2019-03-12 22:20:32 +0100

    so zeug

--- src/team.doma/Main.java
+++ src/team.doma/Main.java
@@ -1,17 +1,37 @@
 package team.doma;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
     public static void main(String args[]){
 
+
         // Text des Users einlesen
         JFormattedTextField amountField;
 
         amountField = new JFormattedTextField(100);
-        amountField.setValue(new Double(50));
+        amountField.setValue(50);
         amountField.setColumns(10);
 //        amountField.addPropertyChangeListener("value", this);
          }
+         //setup Methode
+    public void setUpJframe(int width, int height,Component... comp){
+        //neues Jframe erstellen
+        JFrame jFrame = new JFrame();
+
+        //Titel des Jframes setzen
+        jFrame.setTitle("Application");
+
+        //loop so lange bis alle components drinne sind
+        for(int i = 0; i < comp.length; i++ ){
+            jFrame.add(comp[i]);
+        }
+        //Grösse einstellen
+        jFrame.setSize(width ,height);
+
+        //Jframe sichtbar machen
+        jFrame.setVisible(true);
+    }
 }
