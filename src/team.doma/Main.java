61a4e6c48f77c153bb3d915fa320b9c7d80cec44
mabiffm <martinbirkel@hotmail.com>
2019-03-12 22:09:20 +0100

    first try

--- src/team.doma/Main.java
+++ src/team.doma/Main.java
@@ -1,10 +1,17 @@
 package team.doma;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String args[]){
 
-        ;
+        // Text des Users einlesen
+        JFormattedTextField amountField;
 
-    }
+        amountField = new JFormattedTextField(100);
+        amountField.setValue(new Double(50));
+        amountField.setColumns(10);
+//        amountField.addPropertyChangeListener("value", this);
+         }
 }
